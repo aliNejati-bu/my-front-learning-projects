55e2c915498d952ec698dc30d03699b2e9845afd
aliNejati-bu <electrocellco@gmail.com>
2022-05-28 16:39:38 +0430

    009 add load animation.

--- 009-404page/js/main.js
+++ 009-404page/js/main.js
@@ -2,18 +2,35 @@ let menu = document.getElementById("nav-menu"),
     menu_toggle = document.getElementById("nav-toggle"),
     navClose = document.getElementById("nav-close");
 
-menu_toggle.addEventListener("click",function (){
+menu_toggle.addEventListener("click", function () {
     menu.classList.add("nav__open_menu");
 });
 
 
-
-navClose.addEventListener("click",()=>{
+navClose.addEventListener("click", () => {
     menu.classList.remove("nav__open_menu");
 })
 
-document.querySelectorAll(".nav__menu_item").forEach(ele=>{
-    ele.addEventListener("click",()=>{
+document.querySelectorAll(".nav__menu_item").forEach(ele => {
+    ele.addEventListener("click", () => {
         menu.classList.remove("nav__open_menu");
     });
 })
+
+const sr = ScrollReveal({
+    distance: "90px",
+    duration: 3000
+});
+
+sr.reveal('.home__data', {
+    origin: 'top',
+    delay: 400
+});
+sr.reveal('.home__ghost', {
+    origin: 'bottom',
+    delay: 400
+});
+sr.reveal('.footer', {
+    origin: 'bottom',
+    delay: 500
+});
