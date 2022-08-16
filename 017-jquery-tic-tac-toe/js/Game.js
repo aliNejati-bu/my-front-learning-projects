e3c401394946d2ee1d6e47aade0394eceeae3657
aliNejati-bu <electrocellco@gmail.com>
2022-08-16 22:19:33 +0430

    017 add gameCore Class.

--- 017-jquery-tic-tac-toe/js/Game.js
+++ 017-jquery-tic-tac-toe/js/Game.js
@@ -1,8 +1,143 @@
 class Game {
-    constructor() {
-        this.$gmae = $("#ground .game");
+    constructor(gamePlayedCb, endGameCb) {
         this.status = "blue";
+
+
+        this.gameStatus = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
+
+        this.gamePlayCb = gamePlayedCb;
+
+        this.endGameCb = endGameCb;
+
+        this.endGameStatus = "";
     }
+
+
+    playAGame(row, col) {
+        if (this.gameStatus[row][col] === 0 || this.endGameStatus !== "") {
+
+            return false;
+        }
+
+        if (this.status === "blue") {
+            this.status = "red";
+            this.gameStatus[row][col] = 1;
+        } else {
+            this.status = "blue";
+            this.gameStatus[row][col] = 2;
+        }
+
+        this.gamePlayCb.apply(this, this.gameStatus);
+
+
+        return true;
+    }
+
+    endGameCheck(color) {
+        if (
+            game.gameStatus[0][0] === color &&
+            game.gameStatus[0][1] === color &&
+            game.gameStatus[0][2] === color
+        ) {
+            return {
+                status: [
+                    [color, color, color],
+                    [0, 0, 0],
+                    [0, 0, 0]
+                ]
+            };
+        } else if (
+            game.gameStatus[1][0] === color &&
+            game.gameStatus[1][1] === color &&
+            game.gameStatus[1][2] === color
+        ) {
+            return {
+                status: [
+                    [0, 0, 0],
+                    [color, color, color],
+                    [0, 0, 0]
+                ]
+            };
+        } else if (
+            game.gameStatus[2][0] === color &&
+            game.gameStatus[2][1] === color &&
+            game.gameStatus[2][2] === color
+        ) {
+            return {
+                status: [
+                    [0, 0, 0],
+                    [0, 0, 0],
+                    [color, color, color]
+                ]
+            };
+        } else if (
+            game.gameStatus[0][0] === color &&
+            game.gameStatus[1][1] === color &&
+            game.gameStatus[2][2] === color
+        ) {
+            return {
+                status: [
+                    [color, 0, 0],
+                    [0, color, 0],
+                    [0, 0, color]
+                ]
+            };
+        } else if (
+            game.gameStatus[0][2] === color &&
+            game.gameStatus[1][1] === color &&
+            game.gameStatus[2][0] === color
+        ) {
+            return {
+                status: [
+                    [0, 0, color],
+                    [0, color, 0],
+                    [color, 0, 0]
+                ]
+            };
+        } else if (
+            game.gameStatus[0][0] === color &&
+            game.gameStatus[1][0] === color &&
+            game.gameStatus[2][0] === color
+        ) {
+            return {
+                status: [
+                    [color, 0, 0],
+                    [color, 0, 0],
+                    [color, 0, 0]
+                ]
+            };
+        } else if (
+            game.gameStatus[0][1] === color &&
+            game.gameStatus[1][1] === color &&
+            game.gameStatus[2][1] === color
+        ) {
+            return {
+                status: [
+                    [0, color, 0],
+                    [0, color, 0],
+                    [0, color, 0]
+                ]
+            };
+        } else if (
+            game.gameStatus[0][2] === color &&
+            game.gameStatus[1][2] === color &&
+            game.gameStatus[2][2] === color
+        ) {
+            return {
+                status: [
+                    [0, 0, color],
+                    [0, 0, color],
+                    [0, 0, color]
+                ]
+            };
+        }
+    }
+
+
 }
 
 let game = new Game();
